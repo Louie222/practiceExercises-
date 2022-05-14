4c05a3297cf0af2438f3518fc2b3d82986d28e5b
Louie Obaldo <louie.obaldo@education.wa.edu.au>
2022-05-14 21:42:52 +1000

    updating

--- Javascript/Day-01/04-Stu_Hello-Variable/Unsolved/script.js
+++ Javascript/Day-01/04-Stu_Hello-Variable/Unsolved/script.js
@@ -1,1 +1,18 @@
 // Write Your JavaScript Code Here
+var name = "Louie";
+console.log("My name is" ,name);
+
+var pets = "1";
+console.log("I have" ,pets);
+
+var funfact = "I hate coriander";
+console.log("Fun fact:", funfact);
+
+name = "Tristan and Sarad";
+console.log("My name is", name);
+
+pets = "0";
+console.log("I have", pets);
+
+funfact = "I'm a man child";
+console.log("Fun fact:", funfact);
